5114b6e649197fa613c2c7e5b531c7168a59e59f
chea_darika <cheadarika15@gmail.com>
2025-09-29 15:35:10 +0700

    add script insert role

---
+++ src/main/resources/data/script.sql
@@ -0,0 +1,4 @@
+INSERT INTO "public"."role" ("id", "status", "version", "created_at", "updated_at", "name", "role")
+VALUES (1, 't', 0, null, null, 'Admin', 'ADMIN');
+INSERT INTO "public"."role" ("id", "status", "version", "created_at", "updated_at", "name", "role")
+VALUES (2, 't', 0, null, null, 'User', 'USER');
